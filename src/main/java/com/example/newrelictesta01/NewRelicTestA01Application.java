eb0e33163269882e8e5c0a04fa5abec9d5222290
YI Eun Gook <eungook83@gmail.com>
2021-09-10 23:55:42 +0900

    git init

---
+++ src/main/java/com/example/newrelictesta01/NewRelicTestA01Application.java
@@ -0,0 +1,13 @@
+package com.example.newrelictesta01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewRelicTestA01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewRelicTestA01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/newrelictesta01/NewRelicTestA01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newrelictesta01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewRelicTestA01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
